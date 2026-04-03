201d66f7e85d2f4e4c6b45c807ea5e34003e0d59
bambingames-lab <bambingames@gmail.com>
2026-04-02 23:51:28 -0700

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open('norcal-map-v1').then(cache => cache.addAll(['./', './index.html', './style.css', './app.js', './manifest.json'])));
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
-});
