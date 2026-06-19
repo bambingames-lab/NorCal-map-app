94c7d465737811bd4dc155990d44ad07de3c8e45
bambingames-lab <bambingames@gmail.com>
2026-06-18 18:42:05 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Paste your Supabase info here after creating a project.
 // Leave blank to use local-only demo mode.
-window.TM_SUPABASE_URL = "";
-window.TM_SUPABASE_ANON_KEY = "";
+window.TM_SUPABASE_URL = "https://vaftkfhzymyjpgnyvkrk.supabase.co";
+window.TM_SUPABASE_ANON_KEY = "sb_publishable_EoJhZ2dMaiDS-VdGytKl3A_Jj3nQm5g";
